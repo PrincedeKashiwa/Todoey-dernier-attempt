bfbf2a2a4b9c1dd93981183cef07d7402d876df5
BRUNO SMITH <brunosmith@pc114.home>
2018-12-23 17:05:32 +0100

    todo list complete with datasource and delegate methods.

---
+++ Todoey dernier attempt/TodoListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Todoey dernier attempt
+//
+//  Created by BRUNO SMITH on 21/12/2018.
+//  Copyright © 2018 BRUNO SMITH. All rights reserved.
+//
+
+import UIKit
+//don't forget to change the superclass from UIVIEWCONTROLLER TO UITABLEVIEWCONTROLLER
+class TodoListViewController: UITableViewController {
+    let itemArray = ["Find Mike","Buy Eggs","Destroy zaboz"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - TableView Datasource
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    //MARK - TABLEVIEW Delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print (itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)//flashes grey
+        
+    }
+}
+
